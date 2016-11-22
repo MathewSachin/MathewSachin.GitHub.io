4d268e1bc99d26b0c91d39d14183810702098b89
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-11-22 21:13:58 +0530

    update

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+        $('.navbar-toggle:visible').click();
+    });
+});
