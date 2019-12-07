2a26581f91018226d5bd2b419723bcbc77ea0823
Mathew Sachin <mathew.sachin.git@outlook.com>
2019-12-07 16:12:02 +0530

    tooltip

--- scripts/copy-code.js
+++ scripts/copy-code.js
@@ -4,11 +4,12 @@ blocks.each(function(i) {
     var id = "block" + (i + 1);
     $(this).attr('id', id);
 
-    var btn = '<button class="btn btn-info btn-sm my-0 px-2 btn-clip float-right" data-clipboard-target="#' + id + '"><i class="fa fa-copy fa-lg"></i></button>';
+    var btn = '<button class="btn btn-info btn-sm my-0 px-2 btn-clip float-right" data-clipboard-target="#' + id + '" data-toggle="tooltip" data-placement="bottom" title="Copy to clipboard"><i class="fa fa-copy fa-lg"></i></button>';
     
     var code = $(this);
 
     code.prepend(btn);
 });
 
+$('.btn-clip').tooltip()
 new ClipboardJS('.btn-clip');
