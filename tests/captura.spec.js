8216740a5a72a8f1292dc77489a5bdb3ad741f90
Mathew Sachin <MathewSachin@users.noreply.github.com>
2026-04-11 21:28:20 +0530

    disable service worker for captura functionality tests

--- tests/captura.spec.js
+++ tests/captura.spec.js
@@ -9,6 +9,10 @@ import {
 
 test.describe('Captura Web Recorder', () => {
   test.beforeEach(async ({ page }) => {
+    // 1. Block the Service Worker so it doesn't trigger random page reloads
+    // halfway through our tests while trying to update cached assets.
+    await page.route('**/sw.js', route => route.abort());
+
     await page.addInitScript(opfsMockScript);
     await page.goto('/tools/captura/');
   });
